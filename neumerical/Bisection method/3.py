9f4b9f92da41d3ae1d3be83f9b65f99a2e80b267
mdasadur5027 <mdasadur5027@gmail.com>
2024-12-30 10:23:02 +0600

    Create 3.py

---
+++ neumerical/Bisection method/3.py
@@ -0,0 +1,28 @@
+# x^sin2 - 4
+import numpy as np
+
+def f(x):
+    return x**np.sin(2) - 4
+
+a, b = 1, 4
+tol = 1e-6
+
+if f(a)*f(b) > 0:
+        print('The initial guesses do not bracket a root. Bisection cannot proceed.')
+        exit()
+
+for i in range(100):
+    c = (a+b)/2
+
+    if abs(a-b) < tol :
+        break
+
+    if f(a)*f(c) < 0:
+        b = c
+    else:
+        a =c 
+
+x = (a+b)/2
+print(f'Root (Bisection method): {x}')
+    
+    
